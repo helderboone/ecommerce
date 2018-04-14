d11068fabc11a9475cac95b4256cc829844a2dd4
Helder Boone <helder.boone@gmail.com>
2018-04-14 12:46:06 -0300

    Initial commit of Ecommerce app

---
+++ Ecommerce/Data/MySqlDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class MySqlDbContext : DbContext
+    {
+        public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerce/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQty { get; set; }
+    }
+}
