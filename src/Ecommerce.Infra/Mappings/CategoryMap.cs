4d797d1d7ddc96afcfb8e0fb369774d77d8d4c61
Helder Boone <helder.boone@gmail.com>
2019-06-16 01:30:49 -0700

    Set category mapping

---
+++ src/Ecommerce.Infra/Mappings/CategoryMap.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Ecommerce.Infra.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.Id)
+                .HasColumnName("Id");
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(200);
+
+            builder.HasMany(c => c.Products).WithOne(p => p.Category);
+        }
+    }
+}
