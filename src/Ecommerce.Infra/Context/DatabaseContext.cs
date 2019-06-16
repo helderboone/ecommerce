aaf55f6834660172e57f6e8fd728f38801649c92
Helder Boone <helder.boone@gmail.com>
2019-06-16 01:29:53 -0700

    Create database context and initializer

---
+++ src/Ecommerce.Infra/Context/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Infra.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infra.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
