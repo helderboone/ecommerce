9424a5e0b9d4231ab5bd56354aa7bb17b6a42813
Helder Boone <helder.boone@gmail.com>
2019-06-16 01:33:15 -0700

    Create product category

--- src/Ecommerce.Domain/Entities/Product.cs
+++ src/Ecommerce.Domain/Entities/Product.cs
@@ -1,10 +1,14 @@
 ﻿
 namespace Ecommerce.Domain.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
-
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
     }
 }
